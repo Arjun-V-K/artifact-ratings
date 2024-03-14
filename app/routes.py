5939312a28bc1fcb86d9853bc12545eecc4fffcd
Arjun-V-K <vk_arjun@outlook.com>
2024-03-14 19:11:08 +0530

    Fix deleting of substats

--- app/routes.py
+++ app/routes.py
@@ -96,5 +96,6 @@ def delete_artifact(artifact_id):
 @main_bp.route('/delete_all_artifacts', methods=['POST'])
 def delete_all_artifacts():
     Artifact.query.delete()
+    Substat.query.delete()
     db.session.commit()
     return redirect(url_for('main.view_artifacts_page'))
